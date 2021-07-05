7d70a00f3af053fc5f8646c8d8f50861ea290f2c
Your Name <greenfitter516@gmail.com>
2021-07-05 21:03:44 +0600

    list rendering

--- vue/main.js
+++ vue/main.js
@@ -1,4 +1,18 @@
 const app =Vue.createApp({
 
+    data:function(){
 
-});
+        return{
+
+            variants:[
+                {id:1 , name:'Vanilla ',price: '120'},
+                {id:2, name:'Chocolate',price: '100'},
+                {id:3, name:'Mango',price: '150'},
+                {id:4, name:'Strawberry',price: '100'},
+                {id:5, name:'Ornage',price: '100'},
+                {id:5, name:'Apple',price: '100'},
+            ]
+        }
+    }
+
+})
