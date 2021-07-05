381b6d233bf65df65d5a4dd0b41ecde480ed123d
Your Name <greenfitter516@gmail.com>
2021-07-06 00:21:44 +0600

    event

--- vue/main.js
+++ vue/main.js
@@ -7,12 +7,25 @@ const app =Vue.createApp({
             variants:[
                 {id:1 , name:'Vanilla ',price: '120'},
                 {id:2, name:'Chocolate',price: '100'},
-                {id:3, name:'Mango',price: '150'},
-                {id:4, name:'Strawberry',price: '100'},
-                {id:5, name:'Ornage',price: '100'},
-                {id:5, name:'Apple',price: '100'},
-            ]
+                {id:3, name:'Strawberry',price: '150'},
+                {id:4, name:'Orange',price: '100'},
+                {id:5, name:'Lemon',price: '100'},
+            ],
+            cart:[]
+
         }
-    }
+    }, 
+        methods:{
+
+                addToCart(variantId){
+                    this.cart.push(this.variants.find(variant => variant.id =variantId));
+                },
+                removeItem(variantId){
+                    
+                    let remove = this.cart.findIndex(variant =>variant.id =variantId);
+
+                    this.cart.splice(remove, 1);
+                }
+        }
 
 })
